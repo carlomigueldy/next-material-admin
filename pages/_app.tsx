ee4d0e138972313ec522673aafec332c2778bb67
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-03-14 19:47:47 +0800

    feat: installed material-ui implemented with custom theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,44 @@
-import { AppProps } from 'next/dist/next-server/lib/router/router'
-import '../styles/globals.css'
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import { AppProps } from "next/dist/next-server/lib/router/router";
+import Head from "next/head";
+import "../styles/globals.css";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      "Product Sans",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      "Segoe UI",
+      "Roboto",
+      "Oxygen",
+      "Ubuntu",
+      "Cantarell",
+      "Fira Sans",
+      "Droid Sans",
+      "Helvetica Neue",
+      "sans-serif",
+    ].join(","),
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Next Admin</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </Head>
+
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
